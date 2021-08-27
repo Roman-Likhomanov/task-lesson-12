e39bde40e575eeb98e62af57d985f872d0d4a778
Roman Likhomanov <romanlee87@gmail.com>
2021-08-27 12:52:48 +0300

    Setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ["jest"],
+  rules: {
+    "no-param-reassign": "off",
+    "no-plusplus": "off",
+    "default-case": "off",
+    "no-useless-escape": "off",
+    "no-alert": "off",
+    "no-console": "off",
+    "max-len": [
+      "error",
+      {
+        ignoreComments: true,
+      },
+    ],
+    "import/prefer-default-export": "off",
+  },
+};
